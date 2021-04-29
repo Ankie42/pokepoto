55a2c0ea1b1c6da6b0f24f9ac38712d47153f2f1
Anki <elalguien.lo@gmail.com>
2021-04-29 17:52:46 -0400

    generar la tablita

--- js/index.js
+++ js/index.js
@@ -14,18 +14,83 @@ tinymce.init({
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
   });
 
+const pokemonos = [];
 
+const cargarTabla = ()=>{
+  //1. Obtener referencia ed la tabla
+  let tbody = document.querySelector("#tabla-tbody");
+  //Eliminar elementosd del tbody
+  tbody.innerHTML = "";
+
+  //2. Recorrer lista
+  for(let i=0; i < pokemonos.length; ++i){
+    let p = pokemonos[i];
+
+//3. hacer tabla por cada pokeono
+  let tr = document.createElement("tr");  
+
+//4. Por cada atributo /nombre,tipo,desc, etc) se generan celdas (td)
+  let tdNro = document.createElement("td");
+  tdNro.innerText = (i+1);
+  
+  let tdNombre = document.createElement("td");
+  tdNombre.innerText = p.nombre;
+ 
+  let tdTipo = document.createElement("td");
+  let icono = document.createElement("i")
+  if(p.tipo =="fuego"){
+      //<i class="fas fa-burn"></i>
+      icono.classList.add("fas","fa-burn","text-danger","fa-3x");
+  }else if(p.tipo =="planta"){
+    //<i class="fas fa-leaf"></i>
+    icono.classList.add("fas","fa-leaf","text-success","fa-3x")
+  }else if(p.tipo =="agua"){
+    //<i class="far fa-glass-citrus"></i>
+    icono.classList.add("fas","fa-swimmer","text-primary","fa-3x")
+  }else{
+    //<i class="fas fa-user-tie"></i>
+    icono.classList.add("fas","fa-user-tie","text-info","fa-3x")
+  }
+  tdTipo.classList.add("text-center");
+  tdTipo.appendChild(icono);
+  let tdDesc = document.createElement("td");
+  tdDesc.innerHTML = p.descripcion;
+  
+  let tdAcciones = document.createElement("td");
+
+//5. Agrtegar celdas al tr
+    tr.appendChild(tdNro);
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdTipo);
+    tr.appendChild(tdDesc);
+    tr.appendChild(tdAcciones);
+
+//6.Agregar los tr a la tabla
+    tbody.appendChild(tr);
+  }};
 
 document.querySelector("#registrar-btn").addEventListener("click",()=>{
     //value de texts input
     let nombre = document.querySelector("#nombre-txt").value;
     //tener .lo ya escrito
     let descripcion = tinymce.get("descripcion-txt").getContent();
-    //cheked = si radiobutton esta selecc
+    //checked = si radiobutton esta selecc
     let legendario = document.querySelector("#legendario-no").checked;
-
+    
     let tipo = document.querySelector("#tipo-select").value;
 
-    console.log("wena ckbors", nombre,descripcion,legendario,tipo);
-
-});
+    //Como crear un objeto (pokemono se convierte en objeto y puede tener muchos cosas dentro)
+    let pokemono ={};
+    pokemono.nombre = nombre;
+    pokemono.descripcion = descripcion;
+    pokemono.legendario = legendario;
+    pokemono.tipo = tipo;
+    console.log(pokemono);
+    
+    //Guardar en una lista de elemenots
+    pokemonos.push(pokemono); //append
+    cargarTabla();
+   
+    //mensajito bonito (titulo, mensaje, tipo(succ,info,danger,warning))
+    Swal.fire("Exito!", "pokemono registrado","success");
+  });
